e2ebb8be1e34886a696b9b6eb25c0d963ad6bf51
Jonathan Warner <jaxbot@gmail.com>
2015-08-02 23:24:32 -0700

    Add parsing module

--- parser.js
+++ parser.js
@@ -1,14 +1,23 @@
-var data = require("./file.json");
-var html = data['2'].data;
-var vehicles = html.split("<a class=\"vehicle");
-for (var i = 0; i < vehicles.length; i++) {
-  var vehicle = vehicles[i];
-  var price = vehicle.split("<p>$");
-  if (!price || !price[1]) continue;
-  price = price[1].split("</p>")[0];
-  var tagline = vehicle.split("<p>");
-  tagline = tagline[1].split("</p>")[0].split(" | ");
-  var year = tagline[0];
-  var miles = tagline[1];
-  var other = tagline[2];
+module.exports = function(data) {
+  var html = data['2'].data;
+  var vehicles = html.split("<a class=\"vehicle");
+  var vehicles_array = [];
+  for (var i = 0; i < vehicles.length; i++) {
+    var vehicle = vehicles[i];
+    var price = vehicle.split("<p>$");
+    if (!price || !price[1]) continue;
+    price = price[1].split("</p>")[0];
+    var tagline = vehicle.split("<p>");
+    tagline = tagline[1].split("</p>")[0].split(" | ");
+    var year = tagline[0];
+    var miles = tagline[1];
+    var model = tagline[2];
+    vehicles_array.push({
+      year: year,
+      miles: miles,
+      price: price,
+      model: model,
+    });
+  }
+  return vehicles_array;
 }
